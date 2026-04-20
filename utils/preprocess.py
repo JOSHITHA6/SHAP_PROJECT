fd5348c2beb93f64f4ceab3f65258fb6ae57f820
PalavalasaSaiJoshitha <322103382046@gvpce.ac.in>
2026-04-20 23:49:53 +0530

    Updated app.py

--- utils/preprocess.py
+++ utils/preprocess.py
@@ -1,29 +1,26 @@
-# utils/preprocess.py
-
 import pandas as pd
-from sklearn.preprocessing import LabelEncoder, StandardScaler
-
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 
 def preprocess_data(df, target):
-    df = df.copy()
 
-    # ---------------- HANDLE MISSING VALUES ---------------- #
-    df = df.dropna()
+    df = pd.get_dummies(df, drop_first=True)
 
-    # ---------------- SEPARATE FEATURES & TARGET ---------------- #
     X = df.drop(columns=[target])
     y = df[target]
 
-    # ---------------- ENCODE CATEGORICAL FEATURES ---------------- #
-    X = pd.get_dummies(X)
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.3, random_state=42
+    )
 
-    # ---------------- ENCODE TARGET (if classification) ---------------- #
-    if y.dtype == 'object':
-        le = LabelEncoder()
-        y = le.fit_transform(y)
-
-    # ---------------- SCALING ---------------- #
     scaler = StandardScaler()
-    X_scaled = scaler.fit_transform(X)
 
-    return X_scaled, y, scaler
+    X_train = scaler.fit_transform(X_train)
+
+    # Convert back to DataFrame (VERY IMPORTANT for SHAP)
+    X_test = pd.DataFrame(
+        scaler.transform(X_test),
+        columns=X.columns
+    )
+
+    return X_train, X_test, y_train, y_test
