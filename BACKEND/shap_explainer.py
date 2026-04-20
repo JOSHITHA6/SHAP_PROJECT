78c568b45d7c7b479f4b10a0d466dbea8a27c6eb
PalavalasaSaiJoshitha <322103382046@gvpce.ac.in>
2026-04-20 23:07:37 +0530

    Made shap explainer changes

--- BACKEND/shap_explainer.py
+++ BACKEND/shap_explainer.py
@@ -1,22 +1,25 @@
 import shap
 import matplotlib.pyplot as plt
 
-
 def explain_model(model, X, model_type):
-    # Choose correct SHAP explainer
+
+    # 🔥 TAKE SMALL SAMPLE (IMPORTANT)
+    X_sample = X[:50]
+
     if model_type in ["Random Forest", "XGBoost"]:
         explainer = shap.TreeExplainer(model)
+        shap_values = explainer.shap_values(X_sample)
 
     elif model_type in ["Linear Regression", "Logistic Regression"]:
-        explainer = shap.LinearExplainer(model, X)
+        explainer = shap.LinearExplainer(model, X_sample)
+        shap_values = explainer.shap_values(X_sample)
 
     else:
-        explainer = shap.KernelExplainer(model.predict, X)
+        explainer = shap.KernelExplainer(model.predict, X_sample)
+        shap_values = explainer.shap_values(X_sample)
 
-    shap_values = explainer(X)
-
-    # Plot (global explanation)
+    # Plot
     fig, ax = plt.subplots()
-    shap.summary_plot(shap_values, X, show=False)
+    shap.summary_plot(shap_values, X_sample, show=False)
 
     return fig
