87645fe744080e34d6b08ee9cd61f35128eaba81
PalavalasaSaiJoshitha <322103382046@gvpce.ac.in>
2026-04-21 18:38:02 +0530

    Shap explainer updated

--- BACKEND/shap_explainer.py
+++ BACKEND/shap_explainer.py
@@ -5,7 +5,7 @@ def generate_shap_plots(model, X_sample):
 
     # Universal SHAP (works for all models)
     explainer = shap.Explainer(model, X_sample)
-    shap_values = explainer(X_sample)
+    shap_values = explainer(X_sample, check_additivity=False)
 
     # -------- GLOBAL (Beeswarm / Violin-like) --------
     fig_global, ax1 = plt.subplots()
