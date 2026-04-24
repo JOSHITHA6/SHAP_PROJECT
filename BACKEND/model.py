1ea9b34a2d849e5a7237cb24b18c447477968632
PalavalasaSaiJoshitha <322103382046@gvpce.ac.in>
2026-04-24 14:21:32 +0530

    Updated app.py

--- BACKEND/model.py
+++ BACKEND/model.py
@@ -1,19 +1,31 @@
-from sklearn.linear_model import LogisticRegression, LinearRegression
 from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
+from sklearn.linear_model import LinearRegression, LogisticRegression
 
 def train_model(X_train, y_train, task, model_type):
 
+    # ================= CLASSIFICATION =================
     if task == "Classification":
+
         if model_type == "Random Forest":
             model = RandomForestClassifier()
-        else:
-            model = LogisticRegression(max_iter=2000)
 
+        elif model_type == "Logistic Regression":
+            model = LogisticRegression(max_iter=1000)
+
+        elif model_type == "Linear Regression":
+            raise ValueError("Linear Regression cannot be used for Classification")
+
+    # ================= REGRESSION =================
     else:
+
         if model_type == "Random Forest":
             model = RandomForestRegressor()
-        else:
+
+        elif model_type == "Linear Regression":
             model = LinearRegression()
+
+        elif model_type == "Logistic Regression":
+            raise ValueError("Logistic Regression cannot be used for Regression")
 
     model.fit(X_train, y_train)
     return model
