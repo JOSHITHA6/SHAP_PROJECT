8d0cb1883d4100477cb5dfbe4dd380122c7a267f
PalavalasaSaiJoshitha <322103382046@gvpce.ac.in>
2026-04-24 12:44:14 +0530

    Updated shap explainer

--- BACKEND/shap_explainer.py
+++ BACKEND/shap_explainer.py
@@ -1,18 +1,18 @@
 import shap
 import matplotlib.pyplot as plt
 
-def generate_shap_plots(model, X_sample):
+def generate_shap_plots(model, X_sample, X_single):
 
-    # Universal SHAP (works for all models)
     explainer = shap.Explainer(model, X_sample)
-    shap_values = explainer(X_sample, check_additivity=False)
 
-    # -------- GLOBAL (Beeswarm / Violin-like) --------
-    fig_global, ax1 = plt.subplots()
+    # GLOBAL
+    shap_values = explainer(X_sample)
+    fig_global = plt.figure()
     shap.plots.beeswarm(shap_values, show=False)
 
-    # -------- LOCAL (Waterfall) --------
-    fig_local, ax2 = plt.subplots()
-    shap.plots.waterfall(shap_values[0], show=False)
+    # LOCAL
+    shap_single = explainer(X_single)
+    fig_local = plt.figure()
+    shap.plots.waterfall(shap_single[0], show=False)
 
     return fig_global, fig_local
