4a40a9fe964de03db79ad745059ce6bf5fe3f94b
PalavalasaSaiJoshitha <322103382046@gvpce.ac.in>
2026-04-24 23:40:32 +0530

    Updatded preprocess.py

--- utils/preprocess.py
+++ utils/preprocess.py
@@ -1,26 +1,50 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.impute import SimpleImputer
 
-def preprocess_data(df, target):
+def preprocess_data(df, target_col):
 
-    df = pd.get_dummies(df, drop_first=True)
+    X = df.drop(columns=[target_col])
+    y = df[target_col]
 
-    X = df.drop(columns=[target])
-    y = df[target]
+    # Detect column types
+    num_cols = X.select_dtypes(include=['int64', 'float64']).columns
+    cat_cols = X.select_dtypes(include=['object', 'category']).columns
 
+    # Pipelines
+    num_pipeline = Pipeline([
+        ("imputer", SimpleImputer(strategy="mean")),
+        ("scaler", StandardScaler())
+    ])
+
+    cat_pipeline = Pipeline([
+        ("imputer", SimpleImputer(strategy="most_frequent")),
+        ("encoder", OneHotEncoder(handle_unknown='ignore'))
+    ])
+
+    preprocessor = ColumnTransformer([
+        ("num", num_pipeline, num_cols),
+        ("cat", cat_pipeline, cat_cols)
+    ])
+
+    # Split
     X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=0.3, random_state=42
+        X, y, test_size=0.2, random_state=42
     )
 
-    scaler = StandardScaler()
+    # Fit only on train
+    X_train_processed = preprocessor.fit_transform(X_train)
+    X_test_processed = preprocessor.transform(X_test)
 
-    X_train = scaler.fit_transform(X_train)
-
-    # Convert back to DataFrame (VERY IMPORTANT for SHAP)
-    X_test = pd.DataFrame(
-        scaler.transform(X_test),
-        columns=X.columns
+    return (
+        X_train_processed,
+        X_test_processed,
+        y_train,
+        y_test,
+        X_test,        # original test data (for UI)
+        preprocessor,
+        list(X.columns)
     )
-
-    return X_train, X_test, y_train, y_test
